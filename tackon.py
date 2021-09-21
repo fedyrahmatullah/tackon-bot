88f0b78bb26d935d6a033222461353f03bf1430f
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2021-09-22 01:08:58 +0700

    update

--- tackon.py
+++ tackon.py
@@ -29,3 +29,92 @@ def greeting(sentence):
     for word in sentence.split():
         if word.lower() in salam_input:
             return random.choice(respon_salam)
+        
+def response(responUser): 
+    responTackon=''
+    sent_tokens.append(responUser)
+    if 'apa itu jti' in responUser or 'jurusan ti adalah' in responUser  or 'ti polije' in responUser or 'ti' in responUser:
+        responTackon = responTackon+sent_tokens[0]
+        return responTackon
+    elif 'prayudisium' in responUser or 'khs' in responUser:
+        responTackon = responTackon+sent_tokens[8]
+        return responTackon
+
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize)
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    
+    vals = cosine_similarity(tfidf[-1], tfidf) 
+
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2] 
+    
+    if(req_tfidf==0):
+        responTackon=responTackon+"{}".format(random.choice(respon_bingung))
+        return responTackon
+    else:
+        responTackon = responTackon+sent_tokens[idx]
+        return responTackon
+
+def reminder(event, angka, waktu, timer):
+    while timer>0:
+        time.sleep(1)
+        timer-=1
+    toaster = ToastNotifier()  
+    toaster.show_toast("Pengingat", "Tuan, ini sudah {}.\nKegiatan Anda itu {}".format(angka+' '+waktu, ' '.join(event)), icon_path=None, duration=5)
+    while toaster.notification_active(): time.sleep(0.1)
+
+def main(responUser):
+    if(responUser!='bye' or responUser!='dadah' or responUser!='mettu'):
+        if(responUser=='terima kasih' or responUser=='thank you' or responUser=='thanks' or responUser=='suwun' or responUser=="makasih" ):
+            global flag
+            flag=False
+            print("TACKON: sama-sama, selamat beraktifitas ya")
+        else:
+            if len(responUser)==0:
+                print('TACKON:  {}'.format(random.choice(respon_bingung)))
+            elif(greeting(responUser)!=None):
+                print("TACKON: "+greeting(responUser))
+            else:
+                print("TACKON:", ''+response(responUser))
+                sent_tokens.remove(responUser)
+    else:
+        flag=False
+
+
+print("TACKON: Hai Namaku TACKON. Aku bakal bantu kamu ngejawab informasi yang kamu butuhin\n\
+       sebagai mahasiswa TI Polije semester akhir oke?")
+print('TACKON: Aku juga bisa membantumu dalam beberapa hal\n\
+      yaitu ngingetin kamu, coba deh ketik "ingetin saya untuk [aktifitasmu] dalam [waktu] [menit atau jam]"\n\
+      Kalau mau keluar, ketik bye atau dadah atau mettu ya!')
+print('Pesan dari Fedy: TACKON ini ga sempurna guys,\n\
+      jadi ada kemungkinan jawaban dia ganyambung sama pertanyaanmu jadi dimaklumi ya,\n\
+      pastikan juga kamu tanya seputar kegiatan mahasiswa TI semester akhir ya <3')
+
+flag=True
+while(flag==True):
+    responUser = input('->> ')
+    responUser=responUser.lower()
+
+    if 'ingetin saya untuk' in responUser:
+        perintah=responUser.split()
+        try:    
+            if 'menit'==perintah[-1]:
+                timer=int(perintah[-2])*60
+            elif 'jam'==perintah[-1]:
+                timer=int(perintah[-2])*3600
+        except ValueError:
+            print("TACKON: Jangan input angka desimal ya aku gatau guys :(")
+            print("TACKON: inputin bilangan bulat, jangan desimal aku ga ngerti sayang")
+            continue
+        acara=perintah[3:-3]
+        angka=perintah[-2]
+        jenis_waktu=perintah[-1]
+    
+        print("TACKON: oke, tak ingetin buat {} dalam {} {}".format(' '.join(acara), angka, jenis_waktu))  
+        _thread.start_new_thread(reminder,(acara, angka, jenis_waktu,timer)) 
+    else:    
+        main(responUser)
+
+print("TACKON: Bye rek! sampai ketemu nanti, kalo butuh tackon panggil tackon lagi yaa <3") 
